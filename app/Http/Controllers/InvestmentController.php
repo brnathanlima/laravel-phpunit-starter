1d64c1d7e9be0ca725427db24d25326e5b47bb61
Nathanael Lima <nathanaellimacpc@gmail.com>
2021-04-21 13:22:07 -0300

    Adjusting investment storing method

--- app/Http/Controllers/InvestmentController.php
+++ app/Http/Controllers/InvestmentController.php
@@ -8,25 +8,22 @@ use App\Models\Strategy;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-class InvestmentController extends Controller {
-
-    public function index() {
-
+class InvestmentController extends Controller
+{
+    public function index()
+    {
         return $this->successResponse(
             InvestmentResource::collection(Investment::all())
         );
-
     }
 
-    public function store(Request $request) {
-
-        $requestArray = $request->only(
-            [
-                'user_id',
-                'strategy_id',
-                'amount',
-            ]
-        );
+    public function store(Request $request)
+    {
+        $requestArray = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'strategy_id' => 'required|exists:strategies,id',
+            'amount' => 'required',
+        ]);
 
         $successful = (bool)random_int(0, 1);
         $requestArray['successful'] = $successful;
@@ -42,27 +39,25 @@ class InvestmentController extends Controller {
             new InvestmentResource($investment),
             true
         );
-
     }
 
-    public function show(Investment $investment) {
-
+    public function show(Investment $investment)
+    {
         return $this->successResponse(
             new InvestmentResource($investment)
         );
-
     }
 
-    public function update() {
-
+    public function update()
+    {
         return $this->errorResponse(
             'You can\'t update an investment',
             Response::HTTP_UNAUTHORIZED
         );
     }
 
-    public function destroy() {
-
+    public function destroy()
+    {
         return $this->errorResponse(
             'You can\'t delete an investment',
             Response::HTTP_UNAUTHORIZED
